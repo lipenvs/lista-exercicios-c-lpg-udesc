5ac4db20a97f860dc16a7a969b5297a66ed9c994
FELIPE DE ALMEIDA NEVES <15021944738@edu.udesc.br>
2026-04-06 19:52:30 -0300

    adicionado input na questao 1 e criado arquivo da questao 2

--- lista 1/questao1.c
+++ lista 1/questao1.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    float x = 0.0, y = 4.0;
+    float x, y;
+
+    printf("Digite o valor de x: ");
+    scanf("%f", &x);
+    printf("Digite o valor de y: ");
+    scanf("%f", &y);
     
-    if (x == 0 && y == 0){
+    if (x == 0 && y == 0) {
         printf("Origem");
-    } else if ( x > 0 && y > 0) {
+    } else if (x > 0 && y > 0) {
         printf("Q1");
-    } else if ( x < 0 && y < 0) {
+    } else if (x < 0 && y < 0) {
         printf("Q3");
-    } else if ( x > 0 && y < 0) {
+    } else if (x > 0 && y < 0) {
         printf("Q4");
-    } else if ( x < 0 && y > 0 ) {
+    } else if (x < 0 && y > 0) {
         printf("Q2");
     } else if (x == 0 && y != 0) {
         printf("Eixo Y");
-    } else if (x != 0  && y == 0) {
+    } else if (x != 0 && y == 0) {
         printf("Eixo X");
     } else {
         printf("Entrada inválida");
     }
-    
+
     return 0;
 }
